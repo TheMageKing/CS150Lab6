b2d05c3d751e97ab497112ec914aa56dd7dd7f92
Calum McConnell <calumlikesapplepie@gmail.com>
2021-10-17 15:10:03 -0400

    looks like a lot of the code is provided: i'll need to make use
    of whatever prettyfying extensions I can

---
+++ ComplexityCount.java
@@ -0,0 +1,30 @@
+/**
+ * The ComplexityCount interface defines several methods that are used
+ * to manipulate a counter to determine the number of iterations
+ * an algorithm performs for estimating the algorithmic complexity.
+ *
+ * The class will need to specify the counter that is to be used.
+ */
+public interface ComplexityCount {
+  /**
+   * This method will be used to initialize the counter to zero.
+   */
+  public void initialize();
+
+  /**
+ * This method increments the counter by one.
+ */
+ public void incr();
+
+ /**
+ * This will return the counter value.
+ *
+ * @return The returned counter.
+ */
+ public int result();
+
+ /**
+ * This will run the algorithm implement within the class.
+ */
+ public void run_algorithm();
+}
